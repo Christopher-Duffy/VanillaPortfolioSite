d822b360294ce30e264b805f9feba6b2312d988e
Chris Duffy <christopher.b.duffy@gmail.com>
2022-01-03 17:28:19 -0800

    Content Rework

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,1 +1,27 @@
-//not sure if any scripts are needed for this simple site
+document.getElementById("navigationMenu").style.top="0%";
+
+function toggleMenu(){
+    var navMenu=document.getElementById("navigationMenu");
+    if(navMenu.style.top=="0%"){
+        moveNavUp();
+    }else if(navMenu.style.top=="-30%"){
+        moveNavDown();
+    }
+}
+function moveNavUp(){
+    var navMenu=document.getElementById("navigationMenu");
+    if(navMenu.style.top!="-30%"){
+        var newTop=parseInt(navMenu.style.top)-1;
+        navMenu.style.top=newTop+"%";
+        setTimeout(moveNavUp,10);
+    }
+}
+function moveNavDown(){
+    var navMenu=document.getElementById("navigationMenu");
+    if(navMenu.style.top!="0%"){
+        var newTop=parseInt(navMenu.style.top)+1;
+        navMenu.style.top=newTop+"%";
+        setTimeout(moveNavDown,10);
+    }
+    
+}
